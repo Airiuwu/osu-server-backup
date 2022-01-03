fedf4149dbb1484eae94b8e0bf40edf147be87c1
Jacob <61052022+Airiuwu@users.noreply.github.com>
2022-01-03 02:30:58 -0500

    we do a little updating

--- backup.py
+++ backup.py
@@ -1,41 +1,53 @@
-import config, time
 from datetime import date, timedelta
+from time import time, sleep
 from sched import scheduler
-from os import system, path
+from os import system
+import config
 
-s = scheduler(time.time, time.sleep)
+s = scheduler(time, sleep)
+CYAN = '\033[96m'
+ENDC = '\033[0m'
+
+def printConsole(msg):
+    print(f"{CYAN}{msg}{ENDC}")
 
 def backup(sc):
+    system("clear")
     count = 0
-    CYAN = '\033[96m'
-    ENDC = '\033[0m'
 
     # Get today and yesterday's date stamp
-    timeA = date.today()
-    yesterday = timeA - timedelta(days = 1)
-    dateStamp = timeA.strftime("%b-%d-%Y")
-    yesterdayDateStamp = yesterday.strftime("%b-%d-%Y")
+    time_stamp = date.today()
+    today_date = time_stamp.strftime("%b-%d-%Y")
+    yesterday_date = time_stamp - timedelta(days=1)
+    yesterday_stamp = yesterday_date.strftime("%b-%d-%Y")
 
     # Backup MySQL Database
-    print(f"{CYAN}Creating backup of MySQL database{ENDC}")
-    system(f"mysqldump -u root {config.databaseName} > {config.mainDir}/{dateStamp}.sql")
+    printConsole(msg="Creating backup of MySQL database")
+    system(f"mysqldump -u root {config.databaseName} > {config.mainDir}/{today_date}.sql")
 
     # Backup NGINX configuration
-    print(f"{CYAN}Creating backup of NGINX config{ENDC}")
+    printConsole(msg="Creating backup of NGINX config")
     system(f"cp {config.nginxDir} {config.mainDir}")
 
     # Create RAR archive of the whole server
-    print(f"{CYAN}Creating RAR archive of {config.mainDir} ({dateStamp}.rar){ENDC}")
-    if path.exists(f"{config.selfDir}{yesterdayDateStamp}.rar"):
-        system(f"rm {config.mainDir}/{yesterdayDateStamp}.sql")
-        system(f"rm {yesterdayDateStamp}.rar")
-    system(f"rar a {dateStamp}.rar {config.mainDir}")
+    printConsole(f"Creating RAR archive of {config.mainDir} ({today_date}.rar)")
+    system(f"rar a {today_date}.rar {config.mainDir}")
 
     # Upload new backup and delete yesterday's backup
-    print(f"{CYAN}Uploading {dateStamp}.rar to MEGA servers{ENDC}")
-    system(f'megaput --username {config.megaEmail} --password {config.megaPassword} --path=/Root/{dateStamp}.rar --disable-previews "{dateStamp}.rar"')
+    printConsole(f"Uploading {today_date}.rar to MEGA servers")
+    system(f'megaput --username {config.megaEmail} --password {config.megaPassword} --path=/Root/{today_date}.rar --disable-previews "{today_date}.rar"')
+
+    # If we have a backup on MEGA, delete it.
     if count > 0:
-        system(f'megarm --username {config.megaEmail} --password {config.megaPassword} /Root/{yesterdayDateStamp}.rar')
+        printConsole(msg="Deleting yesterday's backup")
+        system(f'megarm --username {config.megaEmail} --password {config.megaPassword} --reload /Root/')
+        system(f'megarm --username {config.megaEmail} --password {config.megaPassword} /Root/{yesterday_stamp}.rar')
+
+    # Delete the current backup from our system to save storage space
+    printConsole(f"Deleting backup from system")
+    system(f"rm {config.mainDir}/{today_date}.sql")
+    system(f"rm {today_date}.rar")
+
     count += 1
     s.enter(86400, 1, backup, (sc,))
 
