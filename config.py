6e12c3362fa76ae3d388eda09e321c29deee93d7
Jacob <61052022+Airiuwu@users.noreply.github.com>
2022-01-03 02:31:11 -0500

    engrish

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 databaseName = '' # Name of database wanting to backup
 mainDir = '' # Directory which should be all stored together e.g. /home/katsumi
 nginxDir = '' # Directory which NGINX config is stored
-selfDir = '' # Directoryy which the python script is at
+selfDir = '' # Directory which the python script is at
 megaEmail = '' # Email to your MEGA account
 megaPassword = '' # Password to your MEGA account
